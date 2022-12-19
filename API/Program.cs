3ed4d794a9e4b1eeceb242c2004e95854443c4ac
Mingxinwen <starwen.cn@gmail.com>
2022-12-19 22:17:02 +1100

    angularSection

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
